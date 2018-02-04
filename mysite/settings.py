9c63b66769e8298b4bc29c354eb9d2a4ad0f637c
tom-foster <19tfoster87@gmail.com>
2018-02-04 23:01:30 +0000

    changed to postgresql database, slight errors encountered and deviates from the tutorial somewhat but overall it works!

--- mysite/settings.py
+++ mysite/settings.py
@@ -77,9 +77,20 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
+    #A bit unusual on this one, slightly fiddly but the tutorial deviates here
+    #Worth correcting.
+    #
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'djangogirls',
+        'USER': 'tfos1',
+    #     'PASSWORD' : '',
+    #     'HOST' : 'localhost',
+    #     'PORT' : '',
     }
 }
 
